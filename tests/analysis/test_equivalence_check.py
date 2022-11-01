439e9b9292dd4dcaf1432316810652b0d0a6bc6e
Legotier <legotier1999@gmail.com>
2022-11-01 11:46:42 +0100

    Add test for equivalence checks

---
+++ tests/analysis/test_equivalence_check.py
@@ -0,0 +1,66 @@
+from probably.pgcl.ast import Program
+from probably.pgcl.compiler import compile_pgcl
+
+from prodigy.analysis.config import ForwardAnalysisConfig
+from prodigy.analysis.equivalence.equivalence_check import check_equivalence
+
+
+def test_equivalence_check():
+    for engine in ForwardAnalysisConfig.Engine._value2member_map_.values():
+        prog = compile_pgcl("""
+            nat x;
+            nat c;
+            nat temp;
+
+            while (x >= 1){
+            {x := 0 } [1/2] {c := c+1}
+            temp :=0
+            }
+        """)
+        assert isinstance(prog, Program)
+
+        inv = compile_pgcl("""
+            nat x;
+            nat c;
+            nat temp;
+
+            if(x >= 1){
+                temp := geometric(1/2)
+                c := c + temp
+                x := 0
+                temp := 0
+            } else {skip}
+        """)
+        assert isinstance(inv, Program)
+        res, _ = check_equivalence(prog, inv,
+                                   ForwardAnalysisConfig(engine=engine))
+
+        prog = compile_pgcl("""
+            nat x;
+            nat c;
+            nat temp;
+            rparam p;
+
+            while (x >= 1){
+            {x := 0 } [1.0-p] {c := c+1}
+            temp :=0
+            }
+        """)
+        assert isinstance(prog, Program)
+
+        inv = compile_pgcl("""
+            nat x;
+            nat c;
+            nat temp;
+            rparam p;
+
+            if(x >= 1){
+                temp := geometric(p)
+                c := c + temp
+                x := 0
+                temp := 0
+            } else {skip}
+        """)
+        assert isinstance(inv, Program)
+        res, _ = check_equivalence(prog, inv,
+                                   ForwardAnalysisConfig(engine=engine))
