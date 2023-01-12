03dd1762bc83d7bf660021b27abdc655d86c5f2d
ThTomate <thtomate@users.noreply.github.com>
2023-01-12 17:25:15 +0100

    static analysis: written vars implementation

--- prodigy/analysis/static/utils.py
+++ prodigy/analysis/static/utils.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 from typing import List
 
-from probably.pgcl import Expr, Var, UnopExpr, Unop, VarExpr, mut_expr_children
+from probably.pgcl import (AsgnInstr, Expr, Instr, Unop, UnopExpr, Var, VarExpr, Walk, WhileInstr, mut_expr_children,
+                           walk_instrs)
 from probably.util.ref import Mut
 
 
@@ -15,4 +16,20 @@ def _vars_of_expr(expr: Expr) -> List[Var]:
     buffer: List[Var] = []
     for child_ref in mut_expr_children(Mut.alloc(expr)):
         buffer.extend(_vars_of_expr(child_ref.val))
+    return buffer
+
+
+def _written_vars(instrs: AsgnInstr | List[Instr]) -> List[Var]:
+    """Generates a list of "written" vars in a list of statements: left hand-side of assignments and guards of loops."""
+    buffer: List[Var] = []
+    if isinstance(instrs, AsgnInstr):
+        buffer.append(instrs.lhs)
+    else:
+        for instr_ref in walk_instrs(Walk.DOWN, instrs):
+            if isinstance(instr_ref.val, AsgnInstr):
+                buffer.append(instr_ref.val.lhs)
+            elif isinstance(instr_ref.val, WhileInstr):
+                # Termination of while loops determines the value of or a relationship between variables of the guard as
+                # the guard is false then.
+                buffer.extend(_vars_of_expr(instr_ref.val.cond))
     return buffer
