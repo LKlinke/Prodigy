cac33a67fa061163c842cca192332815ce825f3e
Lutz Klinkenberg <lutz.klinkenberg@cs.rwth-aachen.de>
2023-02-08 01:31:49 +0100

    examples added

--- benchmark.py
+++ benchmark.py
@@ -6,24 +6,32 @@ Example: python benchmark.py python prodigy/cli.py --engine ginac main pgfexampl
 
 import subprocess
 import sys
-import time
 
 from prodigy.util.color import Style
 
 count = 20
-timeout = 30
+timeout = 90
 
 times = []
 for i in range(count):
-    print(f"\ron iteration {i+1}/{count}...", end="")
-    start = time.perf_counter()
-    result = subprocess.check_output(sys.argv[1:], timeout=timeout)
-    times.append(time.perf_counter() - start)
+    print(f"\ron iteration {i + 1}/{count}...", end="")
+    # Choose timing behavior by commenting out
+    # psi_timings = True
+    psi_timings = False
+    if psi_timings:
+        result = subprocess.check_output(sys.argv[1:], timeout=timeout)
+        if "seconds" not in result.decode().splitlines()[-2]:
+            times.append(float(result.decode().splitlines()[-3].split()[0]))
+        else:
+            times.append(float(result.decode().splitlines()[-2].split()[0]))
+    else:
+        result = subprocess.check_output(sys.argv[1:], timeout=timeout)
+        times.append(float(result.decode().splitlines()[-1].split()[2]))
 print("\r", end="")
 
-print(f"{Style.OKCYAN}min:{Style.RESET} {min(times)} seconds")
-print(f"{Style.OKCYAN}max:{Style.RESET} {max(times)} seconds")
-print(f"{Style.OKCYAN}average:{Style.RESET} {sum(times) / count} seconds")
+print(f"{Style.OKCYAN}min:{Style.RESET} {min(times):.3f} seconds")
+print(f"{Style.OKCYAN}max:{Style.RESET} {max(times):.3f} seconds")
+print(f"{Style.OKCYAN}average:{Style.RESET} {(sum(times) / count):.3f} seconds")
 
 print()
 print(f"{Style.OKCYAN}Output:{Style.RESET}\n{result.decode()}")
---
+++ benchmark.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+for file in dep_bern dueling_cowboys geometric geometric_parameter n_geometric n_geometric_parameterrandom_walk random_walk_parameter trivial_iid
+do
+  echo $file
+  for i in {1..20}
+  do
+    echo -e "1\npgfexamples/equivalence/${file}_invariant.pgcl" | python prodigy/cli.py main pgfexamples/equivalence/$file.pgcl | tail -n 1 |  awk '{ print $3 }' | sed 's/\./,/g'
+  done
+  echo next
+done
