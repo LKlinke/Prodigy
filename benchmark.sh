a2e119bf3e3d31ddf5ca871cfbf6abcccd610465
Lutz Klinkenberg <lutz.klinkenberg@cs.rwth-aachen.de>
2023-06-20 12:47:43 +0200

    added observe(false) example

--- benchmark.sh
+++ benchmark.sh
@@ -4,7 +4,7 @@ do
   echo $file
   for i in {1..20}
   do
-    echo -e "1\npgfexamples/equivalence/${file}_invariant.pgcl" | python prodigy/cli.py main pgfexamples/equivalence/$file.pgcl | tail -n 1 |  awk '{ print $3 }' | sed 's/\./,/g'
+    echo -e "1\npgfexamples/equivalence/${file}_invariant.pgcl" | python prodigy/cli.py $@ main pgfexamples/equivalence/$file.pgcl | tail -n 1 |  awk '{ print $3 }' | sed 's/\./,/g'
   done
   echo next
 done
