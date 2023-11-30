148a832093f6be36e9ca58688d6309ac56227253
Lutz Klinkenberg <lutz.klinkenberg@cs.rwth-aachen.de>
2023-11-30 15:33:11 +0100

    z3 should be ablte to deal with synthesis problem right now.

--- prodigy/analysis/solver/smtz3_solver.py
+++ prodigy/analysis/solver/smtz3_solver.py
@@ -24,6 +24,10 @@ class SMTZ3Solver(Solver):
         self._factory = dist_factory
 
     def solve(self, f: Distribution, g: Distribution) -> Tuple[Optional[bool], List[Dict[sympy.Expr, sympy.Expr]]]:
+        """
+        WARNING: This method currently just tries to find models which are positive FPS by design. We hence do not have completeness
+        of the algorithm for a given template.
+        """
         self.logger.debug("Check %s == %s", f, g)
         s_f, s_g = sympy.S(str(f)), sympy.S(str(g))
         if not (s_f.is_rational_function() and s_g.is_rational_function()):
@@ -37,14 +41,38 @@ class SMTZ3Solver(Solver):
         lhs = f_num * g_denom
         rhs = g_num * f_denom
 
-        goal_function = self._factory.from_expr(str(lhs - rhs).replace("**", "^"),
-                                                f.get_variables() | g.get_variables())
-        formula = parse_expr(" & ".join([f"( 0 <= {prob.replace('**', '^')})" for prob, _ in goal_function]))
-        self.logger.debug("Generated formula %s", formula)
+        numerator_symbols = list(f_num.free_symbols - {sympy.S(var) for var in f.get_variables()})
+        denominator_symbols = sorted(list(f_denom.free_symbols - {sympy.S(var) for var in f.get_variables()}),
+                                     key=lambda x: str(x))
 
-        # create a (variable, type) context and parse these expressions to pysmt.
-        context = TranslationContext({var: Symbol(var, REAL) for var in f.get_symbols() | g.get_symbols()})
-        smt_formula = expr_to_pysmt(context, formula, is_expectation=True)
+        # create a (variable, type) context for parsing to pySMT
+        context = TranslationContext({var: Symbol(var, REAL) for var in f.get_parameters() | g.get_parameters()})
+
+        # encode positivity (sufficient condition)
+        positivity_of_num = " & ".join(f"0 <= {sym}" for sym in numerator_symbols)
+        positivity_of_denom = " & ".join(
+            f"0 < {sym}" if i == 0 else f"{sym} <= 0" for i, sym in enumerate(denominator_symbols))
+        positivity_of_solution = " & ".join([positivity_of_num, positivity_of_denom])
+        positivity_smt = expr_to_pysmt(context, parse_expr(positivity_of_solution), is_expectation=True)
+
+        # encode invariance condition
+        goal_function = self._factory.from_expr(str(lhs - rhs).replace("**", "^"),
+                                                *f.get_variables() | g.get_variables())
+
+        coefficient_at_state = dict()
+        for prob, state in goal_function:
+            if prob.startswith("-"):
+                prob = "(-1)*" + prob[1:]
+            if coefficient_at_state.get(state):
+                coefficient_at_state[state].append(f"{prob}")
+            else:
+                coefficient_at_state[state] = [f"{prob}"]
+        coefficients = [" + ".join(prob).replace('**', '^') for prob in coefficient_at_state.values()]
+        coefficients = list(
+            map(lambda x: expr_to_pysmt(context, parse_expr(f"0 <= {x}"), is_expectation=True), coefficients))
+        self.logger.debug("Generated formula %s", coefficients)
+
+        smt_formula = And(*coefficients, positivity_smt)
         self.logger.debug("Converted to SMT %s", smt_formula)
 
         start = time.perf_counter()
@@ -59,13 +87,35 @@ class SMTZ3Solver(Solver):
                     if is_sat:
                         partial_model = []
                         sol = {}
+
+                        # Generate solution mapping
                         for k in context.variables.values():
-                            sol[sympy.S(str(k))] = sympy.S(str(solver.get_value(k)))
+                            sol[sympy.S(str(k))] = sympy.S(str(solver.get_value(k)), rational=True)
                             partial_model.append(EqualsOrIff(k, solver.get_value(k)))
-                        if any(sol.values()):
+                        self.logger.debug("Solution found: %s", sol)
+
+                        # exclude division by zero solutioms
+                        if f_denom.subs(sol) == 0:
+                            self.logger.debug("denominator is zero, exclude this solution.")
+                            denom_model = []
+                            for k in context.variables.values():
+                                if sympy.S(str(k)) in denominator_symbols:
+                                    denom_model.append(EqualsOrIff(k, solver.get_value(k)))
+                            solver.add_assertion(Not(And(denom_model)))
+
+                        # exclude numerator is 0 solutions.
+                        elif f_num.subs(sol) == 0:
+                            self.logger.debug("numerator is zero, exclude this solution.")
+                            num_model = []
+                            for k in context.variables.values():
+                                if sympy.S(str(k)) in numerator_symbols:
+                                    num_model.append(EqualsOrIff(k, solver.get_value(k)))
+                            solver.add_assertion(Not(And(num_model)))
+
+                        # might be a good solution
+                        else:
+                            self.logger.debug("Generated model %s", sol)
                             break
-                        solver.add_assertion(Not(And(partial_model)))
-                        self.logger.debug("Generated model %s", sol)
                 except SolverReturnedUnknownResultError:
                     self.logger.debug("Z3 was unable to produce a result.")
                     is_sat = None
