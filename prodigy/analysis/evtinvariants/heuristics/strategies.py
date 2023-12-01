0345f3d1a353dbf094237def5c933e5149ceffd3
Lutz Klinkenberg <lutz.klinkenberg@cs.rwth-aachen.de>
2023-12-01 17:27:10 +0100

    implemented additional positivity heuristics and strategies

--- prodigy/analysis/evtinvariants/heuristics/strategies.py
+++ prodigy/analysis/evtinvariants/heuristics/strategies.py
@@ -2,8 +2,9 @@ from dataclasses import dataclass, field
 from enum import Enum
 from typing import Collection
 
-from prodigy.analysis.evtinvariants.heuristics.positivity import FPSPositivityHeuristic, FPSPositivityHeuristicFactory
-from prodigy.analysis.evtinvariants.heuristics.templates import TemplateHeuristic, TemplateHeuristicsFactory
+from prodigy.analysis.evtinvariants.heuristics.positivity.positivity import FPSPositivityHeuristic, \
+    FPSPositivityHeuristicFactory
+from prodigy.analysis.evtinvariants.heuristics.templates.templates import TemplateHeuristic, TemplateHeuristicsFactory
 from prodigy.distribution import CommonDistributionsFactory
 
 
@@ -29,8 +30,38 @@ class DefaultStrategy(SynthesisStrategy):
         self.positivity_heuristics = FPSPositivityHeuristicFactory.rational_function_denominator_sign(self.dist_factory)
 
 
+@dataclass
+class PartialRational(SynthesisStrategy):
+    variables: Collection[str]
+    dist_factory: CommonDistributionsFactory
+    max_degree: int = 10
+
+    def __post_init__(self):
+        self.template_heuristics = TemplateHeuristicsFactory.rational_functions_total_deg(self.max_degree,
+                                                                                          self.variables,
+                                                                                          self.dist_factory)
+        self.positivity_heuristics = FPSPositivityHeuristicFactory.create_sum_then_rational()
+
+
+@dataclass
+class AllOrPartialRational(SynthesisStrategy):
+    variables: Collection[str]
+    dist_factory: CommonDistributionsFactory
+    max_degree: int = 10
+
+    def __post_init__(self):
+        self.template_heuristics = TemplateHeuristicsFactory.rational_functions_total_deg(self.max_degree,
+                                                                                          self.variables,
+                                                                                          self.dist_factory)
+        self.positivity_heuristics = FPSPositivityHeuristicFactory.create_or(
+            FPSPositivityHeuristicFactory.create_sum_then_rational(),
+            FPSPositivityHeuristicFactory.rational_function_denominator_sign(self.dist_factory))
+
+
 class SynthesisStrategies(Enum):
     DEFAULT = DefaultStrategy
+    SUM_THEN_RATIONAL = PartialRational
+    APART_OR_TOGETHER = AllOrPartialRational
 
     @classmethod
     def make(cls, st: 'SynthesisStrategies', *args, **kwargs) -> SynthesisStrategy:
