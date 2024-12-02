dc32db44f3d5ea2f2d4eb94d20566b0e68774aec
dominikgeissler <dominik.geissler@rwth-aachen.de>
2024-12-02 12:27:33 +0100

    Added missing tests, small fix for monus

--- tests/analysis/solvers/test_mock_solvers.py
+++ tests/analysis/solvers/test_mock_solvers.py
@@ -4,23 +4,24 @@ from prodigy.analysis.solver.solver_type import SolverType
 from prodigy.distribution import CommonDistributionsFactory
 from prodigy.distribution.fast_generating_function import ProdigyPGF
 from prodigy.distribution.generating_function import SympyPGF
+from prodigy.distribution.symengine_distribution import SymenginePGF
 
 
-@pytest.mark.parametrize('factory', [SympyPGF, ProdigyPGF])
+@pytest.mark.parametrize('factory', [SympyPGF, ProdigyPGF, SymenginePGF])
 def test_always_true_solver(factory: CommonDistributionsFactory):
     f = factory.undefined("dummy_var", "another_var")
     g = factory.log("x", "1/2")
     assert SolverType.make(SolverType.TRUE).solve(f, g) == (True, [])
 
 
-@pytest.mark.parametrize('factory', [SympyPGF, ProdigyPGF])
+@pytest.mark.parametrize('factory', [SympyPGF, ProdigyPGF, SymenginePGF])
 def test_always_false_solver(factory: CommonDistributionsFactory):
     f = factory.undefined("dummy_var", "another_var")
     g = factory.log("x", "1/2")
     assert SolverType.make(SolverType.FALSE).solve(f, g) == (False, [])
 
 
-@pytest.mark.parametrize('factory', [SympyPGF, ProdigyPGF])
+@pytest.mark.parametrize('factory', [SympyPGF, ProdigyPGF, SymenginePGF])
 def test_always_none_solver(factory: CommonDistributionsFactory):
     f = factory.undefined("dummy_var", "another_var")
     g = factory.log("x", "1/2")
