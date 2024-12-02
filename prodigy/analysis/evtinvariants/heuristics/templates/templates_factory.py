ed5a17f17bcd150dfca3b0e6069aa0e28e2fa497
Lutz Klinkenberg <lutz.klinkenberg@cs.rwth-aachen.de>
2024-12-02 13:26:45 +0100

    added polynomial heuristic and deleted old instruction handler

--- prodigy/analysis/evtinvariants/heuristics/templates/templates_factory.py
+++ prodigy/analysis/evtinvariants/heuristics/templates/templates_factory.py
@@ -1,11 +1,13 @@
 from enum import Enum
 
+from prodigy.analysis.evtinvariants.heuristics.templates.polynomial_function import PolynomialEnumeration
 from prodigy.analysis.evtinvariants.heuristics.templates.rational_function import RationalFunctionMaxDeg
 from prodigy.analysis.evtinvariants.heuristics.templates.templates import TemplateHeuristic
 
 
 class TemplateHeuristics(Enum):
     DEFAULT = RationalFunctionMaxDeg
+    POLY = PolynomialEnumeration
 
     @staticmethod
     def create(h_type: 'TemplateHeuristics', *args, **kwargs) -> TemplateHeuristic:
