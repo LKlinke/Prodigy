b7e454201b9fdddfcb4ba926f998e7160aee168b
dominikgeissler <dominik.geissler@rwth-aachen.de>
2025-01-11 13:15:33 +0100

    adjusted benchmark script to execute files with provided commands

--- benchmark_comparison.py
+++ benchmark_comparison.py
@@ -5,7 +5,7 @@ import sys
 import sympy as sp
 import os
 from glob import glob
-
+import re
 
 # All files in the pgfexamples folder
 all_files: list[str] = [y for x in os.walk("pgfexamples") for y in glob(os.path.join(x[0], '*.pgcl'))] + [
@@ -13,21 +13,30 @@ all_files: list[str] = [y for x in os.walk("pgfexamples") for y in glob(os.path.
 
 # Timeouts / Exception runs
 # If files are not present, an empty list is returned
-timeouts: list[str] = list(map(str.strip, open("timeouts.txt", "r").readlines())) \
+skip_files: list[str] = list(map(str.strip, open("timeouts.txt", "r").readlines())) \
     if os.path.isfile("timeouts.txt") else [] + \
-    list(map(str.strip, open("exceptions.txt", "r").readlines())) if os.path.isfile("exceptions.txt") else []
+                                           list(map(str.strip,
+                                                    open("exceptions.txt", "r").readlines())) if os.path.isfile(
+    "exceptions.txt") else []
 
 # All available engines
 # (will be executed in this order)
-engines = [
+engines: list[str] = [
     "ginac", "symengine", "sympy"
 ]
+
+# Default CLI args for files that do not contain any additional information
+default_instruction: list[str] = ["main"]
+
+# https://stackoverflow.com/a/14693789
+ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
 
 
 class Run:
     """
     Represents a single run of one engine on one file.
     """
+
     def __init__(self, time, output, file):
         """
         Initializes the Run object.
@@ -60,6 +69,7 @@ class Configuration:
     """
     Represents the configuration given by the user
     """
+
     def __init__(self, args):
         """
         Initializes the Configuration object.
@@ -123,7 +133,7 @@ def create_parser() -> argparse.ArgumentParser:
         "--engine",
         metavar="ENGINE",
         help="The engine that should be tested primarily. Separate multiple engines by ','. " +
-             "If unset, all engines are tested. Note: Engines will be executed in the order given. "+
+             "If unset, all engines are tested. Note: Engines will be executed in the order given. " +
              f"Supported engines: {', '.join(engines)}."
     )
 
@@ -210,42 +220,55 @@ def benchmark(config: Configuration):
 
     # If output is wanted, set up the corresponding file
     if config.output_file is not None:
-        setup_outfile(config.output_file)
+        setup_outfile(config.output_file, config.engine)
 
     counter = 0
     # For each file, test all engines
     for file in config.files:
         counter += 1
         print(f"Now testing: {file} ({counter}/{len(config.files)})")
-
         # Check if the current file is in timeouts
-        if file in timeouts and config.skip_timeouts:
-            print(f"File is in timeouts, skipping...")
+        if file in skip_files and config.skip_timeouts:
+            print(f"File is in skipped files, skipping...")
             continue
         engine_counter = 0
+
+        instructions = obtain_instructions(file)
+        skipped = False
         # For each engine, run the program
         for engine in config.engine:
+            if skipped:
+                continue
+
             engine_counter += 1
             print(f"Running {engine} ({engine_counter}/{len(config.engine)})")
 
             # Check if current file is in timeouts
             # Files can be added dynamically, hence the second check
-            if file in timeouts and config.skip_timeouts:
-                print(f"File is in timeouts, skipping...")
+            if file in skip_files and config.skip_timeouts:
+                print(f"File is in skipped files, skipping...")
+                skipped = True
                 continue
 
             output = ""
+
+            # Check if file is to be skipped
+            if not instructions:
+                print("File marked to be skipped...")
+                skipped = True
+                continue
             # Execute the program
+
+            cmd = ["python", "prodigy/cli.py", "--engine", engine, *instructions]
             try:
-                cmd = ["python", "prodigy/cli.py", "--engine", engine, "main", file]
                 output = subprocess.check_output(cmd, timeout=config.timeout).decode()
-                print(output)
             except TimeoutExpired as e:
                 # Command timed out
                 with open("timeouts.txt", "a") as f:
                     # Write the file in "timeouts.txt" and add to the list
                     f.write(file + "\n")
-                    timeouts.append(file)
+                    skip_files.append(file)
+                    skipped = True
                     if config.fail_on_error:
                         raise e
                     continue
@@ -254,7 +277,8 @@ def benchmark(config: Configuration):
                 with open("exceptions.txt", "a") as f:
                     # Write the file in "timeouts.txt" and add to the list
                     f.write(file + "\n")
-                    timeouts.append(file)
+                    skip_files.append(file)
+                    skipped = True
                     if config.fail_on_error:
                         raise e
                     continue
@@ -263,121 +287,218 @@ def benchmark(config: Configuration):
                 raise RuntimeError("No output was captured.")
 
             # Parse output
+            print(output)
             output = output.splitlines()
+            run = capture_output(output, instructions, file)
+            print(run)
 
-            # Sometimes other stuff is logged, which is captured
-            # but not interesting for the analysis.
-            # The interesting part begins with "Result: [...]"
-            while "Result" not in output[0]:
-                output = output[1:]
-
-            # Add time to the dictionary
-            times[engine].append(
-                Run(
-                    time=float(output[-1].split()[-2]),
-                    output=tuple(
-                        str(
-                            output[0]
-                            .split("\x1b")[2]
-                            .split("[92m")[1]
-                        ).removeprefix("(")
-                        .removesuffix(")")
-                        .split(",")
-                    ),  # Very hacky lol
-                    file=file
-                )
-            )
+            # Add the result to the dictionary
+            times[engine].append(run)
+        fail = False
 
         # Compare results if at least two engines are selected and file wasn't skipped once (if skip_timeouts is set)
-        if len(config.engine) > 1 and (not config.skip_timeouts or file not in timeouts):
-            try:
-                results: dict[str, list[sp.Expr]] = {}
-                for engine in config.engine:
-                    # Results for "engine" look like this:
-                    #   expr, error_prob
-                    results[engine] = [sp.S(times[engine][-1].output[i]) for i in range(2)]
-            except Exception as e:
-                # Something went wrong while parsing
-                print(str(e))
-                if config.fail_on_error:
-                    raise e
-                continue
-
-            fail = False
-            # Compare all results
-            for engine in config.engine:
-                if fail:
-                    break
-                # Engines the current engine is compared against
-                # Technically, we only need one direction of equality
-                # but this is more convenient
-                other_engines = set(results.keys()) - {engine}
-                for other_engine in other_engines:
-                    if fail:
-                        break
-                    # Compare expr and error_prob
-                    for i in range(2):
-                        if fail:
-                            break
-                        try:
-                            assert results[engine][i].equals(results[other_engine][i]), \
-                                f"""
-                                Engine {engine} disagrees with engine {other_engine} on file {file}.
-                                """ + "\n".join(f"{e}: {results[e][i]}" for e in engines)
-                        except AssertionError as e:
-                            with open("exceptions.txt", "a") as f:
-                                # Write file to exception file
-                                # We do not need to add file to timeouts,
-                                # as the file was already checked
-                                f.write(file + "\n")
-                            print(str(e))
-                            if config.fail_on_error:
-                                raise e
-                            fail = True
-                            break
-
-            # Results are equal, add run to output file (if set)
-            if config.output_file is not None and not fail:
-                with open(config.output_file, "a") as f:
-                    f.write(file)
-                    for engine in config.engine:
-                        f.write(f",{times[engine][-1].time}")
-                    f.write("\n")
-
+        if len(config.engine) > 1 and not skipped:
+            fail = compare_output({engine: times[engine][-1] for engine in config.engine}, instructions, file, config.fail_on_error)
             if not fail:
                 print("Results are equal, continuing...")
 
+        # Write results if output file is set (and results are equal or just one engine is tested)
+        if config.output_file is not None and not fail and not skipped:
+            with open(config.output_file, "a") as f:
+                f.write(file)
+                for engine in config.engine:
+                    f.write(f",{times[engine][-1].time}")
+                f.write("\n")
     # If generate markdown is set, create the Markdown table
     if config.generate_markdown:
         print("Generating markdown table...")
         generate_markdown_table(config.output_file, config.engine)
 
 
-def setup_outfile(out_file: str):
+def setup_outfile(out_file: str, engine_list: list[str]) -> None:
     # If file exists, rename it to filename_{i}.extension
     if os.path.isfile(out_file):
         i = 1
-        new_name = out_file.split(".")[0] + f"_{i}." + out_file.split(".")[1]
+        new_name_template = lambda el: out_file.split(".")[0] + f"_{el}." + out_file.split(".")[1]
+        new_name = new_name_template(i)
         while os.path.isfile(new_name):
             i += 1
-            new_name = out_file.split(".")[0] + f"_{i}." + out_file.split(".")[1]
+            new_name = new_name_template(i)
         os.rename(out_file, new_name)
 
     # Write header
     with open(out_file, "a") as f:
         f.write("file")
-        for engine in engines:
+        for engine in engine_list:
             f.write(f",{engine}")
         f.write("\n")
 
 
-def generate_markdown_table(csv_file: str, engine_list: list[str]):
+def obtain_instructions(file_path: str) -> list[str]:
     """
-    Parses a given csv file to a Markdown table while highlighting the fastest result
+    Reads the first line of given file and checks whether it is instructions for the command
+    """
+
+    with open(file_path, "r") as f:
+        first_line = f.readlines()[0]
+
+    if first_line.startswith("#"):
+        # First line is an instruction
+        parts = list(map(str.strip, first_line.split()[1:]))
+        if parts == ["skip"]:
+            return []
+        return parts
+    else:
+        # No instruction is given -> return the default instruction
+        print(f"No instruction found for {file_path}, executing default instruction...")
+        return default_instruction + [file_path]
+
+
+def capture_output(output: list[str], cmd: list[str], file: str) -> Run:
+    # Remove ANSI
+    output = [ansi_escape.sub("", o) for o in output]
+    if "main" in cmd:
+        # Sometimes other stuff is logged, which is captured
+        # but not interesting for the analysis.
+        # The interesting part begins with "Result: [...]"
+        while "Result" not in output[0]:
+            output = output[1:]
+        return Run(
+            time=float(output[-1].split()[-2]),
+            output=tuple(
+                str(output[0].split("\t")[1])
+                .removeprefix("(")
+                .removesuffix(")")
+                .split(",")
+            ),
+            file=file
+        )
+    elif "check_equality" in cmd:
+        # TODO check parameter for parameter synthesis?
+        while "equivalent" not in output[0]:
+            output = output[1:]
+        return Run(
+            time=float(output[-1].split()[-2]),
+            output=(output[0].startswith("Program is equivalent to invariant")),
+            file=file
+        )
+    elif "invariant_synthesis" in cmd:
+        while "Invariant: " not in output[0]:
+            output = output[1:]
+        return Run(
+            time=float(output[-1].split()[-2]),
+            output=output[0].split()[1],
+            file=file
+        )
+    else:
+        # todo other methods
+        pass
+
+
+def compare_output(outputs: dict[str, Run], cmd: list[str], file: str, fail_on_error: bool) -> bool:
+    fail = False
+    if "main" in cmd:
+        parsed_results: dict[str, tuple[sp.Expr, sp.Expr]] = {}
+        for engine in outputs.keys():
+            try:
+                parsed_results[engine] = (sp.S(outputs[engine].output[0]), sp.S(outputs[engine].output[0]))
+            except Exception as e:
+                print(str(e))
+                if fail_on_error:
+                    raise e
+
+        for engine in outputs.keys():
+            other_engines = set(outputs.keys()) - {engine}
+            for other_engine in other_engines:
+                if fail:
+                    break
+                for i in range(2):
+                    if fail:
+                        break
+                    try:
+                        assert parsed_results[engine][i].equals(parsed_results[other_engine][i]), \
+                            f"""
+                                Engine {engine} disagrees with {other_engine} on file {file}.
+                            """ + "\n".join(f"{e}: {parsed_results[e][i]}" for e in engines)
+                    except AssertionError as e:
+                        fail = True
+                        with open("exceptions.txt", "a") as f:
+                            # Write file to exception file
+                            # We do not need to add file to timeouts,
+                            # as the file was already checked
+                            f.write(file + "\n")
+                        print(str(e))
+                        if fail_on_error:
+                            raise e
+                        break
+    elif "equivalence" in cmd:
+        for engine in outputs.keys():
+            other_engines = set(outputs.keys()) - {engine}
+            for other_engine in other_engines:
+                if fail:
+                    break
+                try:
+                    assert outputs[engine].output == outputs[other_engine].output, \
+                        f"""
+                            Engine {engine} disagrees with {other_engine} on file {file}.
+                        """ + "\n".join(f"{e}: {outputs[e].output}" for e in engines)
+                except AssertionError as e:
+                    fail = True
+                    with open("exceptions.txt", "a") as f:
+                        # Write file to exception file
+                        # We do not need to add file to timeouts,
+                        # as the file was already checked
+                        f.write(file + "\n")
+                    print(str(e))
+                    if fail_on_error:
+                        raise e
+                    break
+    elif "invariant_synthesis" in cmd:
+        parsed_results: dict[str, sp.Expr] = {}
+        for engine in outputs.keys():
+            try:
+                parsed_results[engine] = sp.S(outputs[engine].output)
+            except Exception as e:
+                print(str(e))
+                if fail_on_error:
+                    raise e
+        for engine in outputs.keys():
+            other_engines = set(outputs.keys()) - {engine}
+            for other_engine in other_engines:
+                if fail:
+                    break
+                try:
+                    assert parsed_results[engine].equals(parsed_results[other_engine]), \
+                        f"""
+                            Engine {engine} disagrees with {other_engine} on file {file}.
+                        """ + "\n".join(f"{e}: {parsed_results[e]}" for e in engines)
+                except AssertionError as e:
+                    fail = True
+                    with open("exceptions.txt", "a") as f:
+                        # Write file to exception file
+                        # We do not need to add file to timeouts,
+                        # as the file was already checked
+                        f.write(file + "\n")
+                    print(str(e))
+                    if fail_on_error:
+                        raise e
+                    break
+    else:
+        # TODO other methods
+        pass
+    return fail
+
+
+def generate_markdown_table(csv_file: str, engine_list: list[str]) -> None:
+    """
+    Parses a given csv file to a Markdown table while highlighting the fastest result.
 
     :param csv_file: The csv file to be parsed.
     :param engine_list: A list of engines to be compared.
     """
+
+    # TODO should number of skipped runs be logged?
+
     out_file = csv_file.split(".")[0] + "_format.md"
 
     # Read the output file
@@ -416,7 +537,8 @@ def generate_markdown_table(csv_file: str, engine_list: list[str]):
                 # Write the run
                 f.write("|" + "|".join(line) + "|\n")
         # Write the average and summary
-        f.write("|Average|" + "|".join([f"{sum(results[i]) / len(results[i])}" for i in range(len(engine_list))]) + "|\n")
+        f.write(
+            "|Average|" + "|".join([f"{sum(results[i]) / len(results[i])}" for i in range(len(engine_list))]) + "|\n")
         f.write("Times fastest run|" + "|".join(map(str, no_fastest)) + "|\n")
 
 
