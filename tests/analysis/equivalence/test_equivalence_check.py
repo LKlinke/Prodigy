7b1eb64dbcf4c2fdc262a0d4718328e3e2b64646
Dominik <dominik.geissler@rwth-aachen.de>
2025-02-05 10:14:50 +0100

    added mockup test for new equality check

--- tests/analysis/equivalence/test_equivalence_check.py
+++ tests/analysis/equivalence/test_equivalence_check.py
@@ -1,3 +1,5 @@
+import builtins
+
 import pytest
 import sympy
 from probably.pgcl.ast import Program
@@ -37,6 +39,7 @@ def test_equivalence_check(engine):
         } else {skip}
     """)
     assert isinstance(inv, Program)
+
     res, subs = check_equivalence(prog, inv, ForwardAnalysisConfig(engine=engine), compute_semantics)
     assert res
     assert subs == []
@@ -77,3 +80,31 @@ def test_equivalence_check_parameter(engine):
     assert res
     assert len(subs) == 1
     assert sympy.S(subs[0][sympy.S('p')]) == sympy.S('0.5') or sympy.S(subs[0][sympy.S('p')]) == sympy.S('1/2')
+
+@pytest.mark.parametrize(
+    'engine',
+    [ForwardAnalysisConfig.Engine.GINAC, ForwardAnalysisConfig.Engine.SYMPY,
+     ForwardAnalysisConfig.Engine.SYMENGINE])
+def test_equivalence_file(monkeypatch, engine):
+    # Read the body of the program files
+    with open("pgfexamples/equivalence/loop_free/bernoulli.pgcl", "r") as f:
+        file = "\n".join(f.readlines())
+
+    with open("pgfexamples/equivalence/loop_free/bernoulli2.pgcl", "r") as f:
+        inv = "\n".join(f.readlines())
+
+    # Compile them to a program object
+    prog1 = compile_pgcl(file)
+    prog2 = compile_pgcl(inv)
+
+    assert(isinstance(prog1, Program))
+    assert(isinstance(prog2, Program))
+
+    # Simulate the input for the invariant files
+    monkeypatch.setattr(builtins, "input", lambda _: "pgfexamples/equivalence/loop_free/bernoulli2.pgcl")
+
+    # Run the main program
+    res, subs = check_equivalence(prog1, prog2, ForwardAnalysisConfig(engine=engine), compute_semantics)
+    assert res
+    assert subs == []
+
